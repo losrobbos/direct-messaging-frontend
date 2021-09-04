716ea41475e725a0da2b8dc2c882a6628b3b7861
losrobbos <losrobbos@web.de>
2021-09-04 11:33:00 +0200

    Printout of ENV variables

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 export const MESSAGE_SERVER_URL = process.env.MESSAGE_SERVER_URL || "http://localhost:5000/"
 
+console.log("API URL: ", MESSAGE_SERVER_URL)
+
 const axiosGlobal = axios.create({
   baseURL: MESSAGE_SERVER_URL
 })
