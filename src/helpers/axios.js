b090696b5dc599dcc850a0a861f5f4e1ffb0bc72
losrobbos <losrobbos@web.de>
2021-09-04 11:36:13 +0200

    Outsourced API url to env

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-export const MESSAGE_SERVER_URL = process.env.MESSAGE_SERVER_URL || "http://localhost:5000/"
+export const MESSAGE_SERVER_URL = process.env.REACT_APP_MESSAGE_SERVER_URL || "http://localhost:5000/"
+// export const MESSAGE_SERVER_URL = process.env.MESSAGE_SERVER_URL
 
 console.log("API URL: ", MESSAGE_SERVER_URL)
 
